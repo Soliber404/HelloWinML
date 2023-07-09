ad04623f07a1b2b3521117511c3d9be80f2b6f76
lucas.lv <lvjinchi404@outlook.com>
2023-07-09 15:58:28 +0800

    添加教程代码
    https://learn.microsoft.com/zh-cn/windows/ai/windows-ml/get-started-desktop

---
+++ pch.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <winrt/Windows.AI.MachineLearning.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Graphics.Imaging.h>
+#include <winrt/Windows.Media.h>
+#include <winrt/Windows.Storage.h>
+
+#include <string>
+#include <fstream>
+
+#include <Windows.h>
